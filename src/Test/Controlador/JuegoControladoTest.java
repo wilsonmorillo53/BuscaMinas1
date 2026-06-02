ddf8c734292b1961d4ad8ab418dc8050f7400036
wilsonmorillo53 <wilsonmorillo53@gmail.com>
2026-06-02 00:55:29 -0500

    ya de agrego

---
+++ src/Test/Controlador/JuegoControladoTest.java
@@ -0,0 +1,12 @@
+package com.buscaminas.controlador;
+
+import org.junit.jupiter.api.*;
+import static org.mockito.Mockito.*; // Requiere Mockito en classpath
+
+class JuegoControladorTest {
+    // Se pueden mockear vista y gestor para pruebas aisladas
+    @Test
+    void testProcesarComandoValido() {
+        // Implementación para segundo commit
+    }
+}
